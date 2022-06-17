361ee9ae4874487ddeec4b96ede0db2411159a67
mariapereztorrealba <102602710+desarrollo-branar@users.noreply.github.com>
2022-06-17 11:44:56 -0400

    nueva
    nueva

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">www.branar.com</i>',
+		'<i class="mascota">Servicios en tecnología</i>',
+		'<i class="mascota">Calidad y servicio</i>',
+		'<i class="mascota">Marketing digital y desarrollo web</i>',
+		'<i class="mascota">Centro de Servicio Autorizado Epson</i>',	
+		'<i class="mascota">Servicio Técnico</i>',
+		'<i class="mascota">Soporte de sistemas</i>',
+		'<i class="mascota">Servidores virtuales</i>',
+		'<i class="mascota">Hosting y correos corporativos</i>'
+		
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
