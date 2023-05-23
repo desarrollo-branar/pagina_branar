55c4acdaffcb847d10019888cc17793a29ba4a37
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-23 10:18:01 -0400

    acordion del menu-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,9 @@ window.addEventListener('scroll', function() {
 
 });
 
-
+/** 
+ * Evento para desplegar mobile-navbar
+*/
 const menuHamburguer = document.getElementById('icon-hamburguer');
 
 menuHamburguer.addEventListener('click', function() {
@@ -27,4 +29,29 @@ menuHamburguer.addEventListener('click', function() {
         mobileNavbar.style.left = '-260px';
         icon.setAttribute('name','reorder-four-outline');
     }
+});
+
+/**
+ * Evento para desplegar submenu de mobile-navbar
+ */
+const submenuItems = document.querySelectorAll('.mobile-navbar .mobile-menu');
+
+submenuItems.forEach(function(item) {
+  item.addEventListener('click', function() {
+    const submenu = this.querySelector('.mobile-submenu');
+    const isExpanded = submenu.style.maxHeight;
+    const icon = this.querySelector('ion-icon');
+
+    submenuItems.forEach(function(item) {
+      item.querySelector('.mobile-submenu').style.maxHeight = null;
+    });
+    
+    if (isExpanded) {
+      submenu.style.maxHeight = null;
+      icon.setAttribute('name', 'caret-down-outline');
+    } else {
+      submenu.style.maxHeight = submenu.scrollHeight + 'px';
+      icon.setAttribute('name', 'caret-up-outline');
+    }
+  });
 });
