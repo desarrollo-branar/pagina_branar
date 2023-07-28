d35409993682355675358bc984237edb160e607a
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-07-28 13:55:15 -0400

    cambiar estilos del main y empezar con socialmedia

---
+++ assets/js/router.js
@@ -0,0 +1,21 @@
+const PATH = {
+    home: {
+        path: '/',
+        template: `
+        <h1>Home</h1>
+        <p>Lorem ipsum diez</p>
+        `
+    },
+    about: {
+        path: '/about',
+        template: `
+        <h1>Sobre mi</h1>
+        `
+    },
+    contact: {
+        path: '/contact',
+        template: `
+        <h1>Contacto</h1>
+        `
+    }
+}
