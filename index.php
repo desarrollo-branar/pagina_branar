f6d3fb1570a35603d024e95a18628b693b89966b
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-11 11:29:50 -0400

    crear rutas de la pagina principal

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Branar\Blog\model\Navegation;
+ 
+$route = new Navegation();
+
+# Pages
+$route->add("/", "pages/index.php");
+$route->add("index/", "pages/index.php");
+$route->add('about/', 'pages/about.php');
+$route->add('contact/', 'pages/contact.php');
+
+# service/
+$route->add('service/csa', 'pages/csa.php');
+$route->add('service/web-development', 'pages/devweb.php');
+$route->add('service/infraestructure', 'pages/infraestructure.php');
+$route->add('service/profitplus', 'pages/profitplus.php');
+$route->add('service/servipack', 'pages/servicepack.php');
+$route->add('service/socialmedia', 'pages/social-media.php');
