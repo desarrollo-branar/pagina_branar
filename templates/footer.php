2098940e4f75a44ef273a6caa5261a090b870a51
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-11 11:28:30 -0400

    crear tmeplates para reutilizar fracciones de codigo

---
+++ templates/footer.php
@@ -0,0 +1,63 @@
+    <footer class="footer">
+        <section class="footer-row">
+            <article class="footer-column">
+                <img src="../assets/images/logo-para-fondo-oscuro.png" class="img-footer" alt="Branar - Servicios en Tecnología">
+                <section class="info-footer">
+                    <p>Nuestra misión es suplir a nuestros clientes con soluciones tecnológicas actualizadas y satisfacer todos sus requerimientos informáticos relacionados con:</p>
+                    <ul>
+                        <li>-El control de los procesos.</li>
+                        <li>-La comunicación.</li>
+                        <li>-Los sistemas de gestión.</li>
+                        <li>-La seguridad de los recursos.</li>
+                    </ul>
+                </section>
+            </article>
+            <article class="footer-column">
+                <h3>Menu</h3>
+                <ul>
+                    <li><a href="" class="underline-footer">Inicio</a></li>
+                    <li><a href="" class="underline-footer">Servicios</a></li>
+                    <li><a href="" class="underline-footer">Productos</a></li>
+                    <li><a href="" class="underline-footer">Nosotros</a></li>
+                </ul>
+            </article>
+            <article class="footer-column">
+                <h3>Servicios</h3>
+                <ul class="submenu">
+                    <li><a href="csa.html" class="underline-footer">Csa Epson <span class="icon-dot"></span></a></li>
+                    <li><a href="infraestructure.html"class="underline-footer">infraestructura <span class="icon-dot"></span></a></li>
+                    <li><a href="social-media.html"class="underline-footer">Marketing <span class="icon-dot"></span></a></li>
+                    <li><a href="software-administrativo.html"class="underline-footer">Profit Plus <span class="icon-dot"></span></a></li>
+                    <li><a href="mantenimiento-computadoras.html"class="underline-footer">Service-Pack <span class="icon-dot"></span></a></li>
+                    <li><a href="servidores.html"class="underline-footer">Virtualización <span class="icon-dot"></span></a></li>
+                </ul>
+            </article>
+            <article class="footer-column">
+                <p><b>Teléfono:</b> (0251)–4453083 / 0412-3290719</p>
+                <p><b>Dirección:</b> Av. Libertador, Cruce Calle 41, Local 41-12. Barquisimeto-Lara.</p>
+                <p><b>Correo:</b> contacto@branar.com</p>
+            </article>
+            <article class="footer-column">
+                <p>&copy; 2022 Branar C.A. Todos los derechos reservados</p>
+            </article>
+        </section>
+    </footer>
+
+    <!-- ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <!-- cdn Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 1500,
+            once: true
+        });
+    </script>
+    <!-- scripts -->
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,66 @@
+    <!-- navbar -->
+    <header class="header">
+        <nav class="navbar">
+            <div class="cont-menu-logo">
+                <a href="../"><img src="../assets/images/navbar-logo.png" alt="" class="menu-logo"></a>
+            </div>
+            <ul class="nav">
+               
+                <li class="menu">
+                    <a href="#" class="line-down underline">Servicios<ion-icon name="caret-down-outline"></ion-icon></a>
+                    <ul class="submenu">
+                        <li><a href="../service/csa" class="underline">Csa Epson<span class="icon-dot"></span></a></li>
+                        <li><a href="../service/infraestructure" class="underline">Infraestructura<span class="icon-dot"></span></a></li>
+                        <li><a href="../service/socialmedia" class="underline">SocialMedia<span class="icon-dot"></span></a></li>
+                        <li><a href="../service/profitplus" class="underline">Profit Plus<span class="icon-dot"></span></a></li>
+                        <li><a href="../service/servipack" class="underline">ServiPack<span class="icon-dot"></span></a></li>
+                        <li><a href="../service/web-development" class="underline">Desarrollo Web<span class="icon-dot"></span></a></li>
+                    </ul>
+                </li>
+                <li class="menu">
+                    <a href="#" class="line-down underline">Productos<ion-icon name="caret-down-outline"></ion-icon></a>
+                    <ul class="submenu">
+                        <li><a href="productos.html#2" class="underline">Equipos <span class="icon-dot"></span></a></li>
+                        <li><a href="productos.html#3" class="underline">Repuestos<span class="icon-dot"></span></a></li>
+                        <li><a href="productos.html#4" class="underline">Consumibles <span class="icon-dot"></span></a></li>
+                        <li><a href="productos.html#5" class="underline">Redes <span class="icon-dot"></span></a></li>
+                        <li><a href="productos.html#7" class="underline">Seguridad <span class="icon-dot"></span></a></li>
+                        <li><a href="productos.html#8" class="underline">Accesorios <span class="icon-dot"></span></a></li>
+                    </ul>
+                </li>
+                <li><a href="../about/" class="underline"><span class="icon-house"></span>Nosotros</a></li>
+                <li><a href="../contact/" class="underline"><span class="icon-mail"></span>Contacto</a></li>
+            </ul>
+            <div class="menu-icon inactive">
+                <ion-icon name="reorder-four-outline" id="icon-hamburguer"></ion-icon>
+            </div>
+        </nav>
+        <div class="mobile-navbar">
+            <ul>
+                <li class="mobile-menu" id="submenu-down">
+                    <a href="#" class="line-down underline" >Servicios<ion-icon name="caret-down-outline"></ion-icon></a>
+                    <ul class="mobile-submenu">
+                        <li><a href="csa.html" class="underline">Csa Epson <span class="icon-dot"></span></a></li>
+                        <li><a href="infraestructure.html" class="underline">Infraestructura <span class="icon-dot"></span></a></li>
+                        <li><a href="social-media.html" class="underline">SocialMedia<span class="icon-dot"></span></a></li>
+                        <li><a href="profitplus.html" class="underline">Profit Plus<span class="icon-dot"></span></a></li>
+                        <li><a href="servipack.html" class="underline">ServiPack<span class="icon-dot"></span></a></li>
+                        <li><a href="devweb.html" class="underline">Desarrollo Web <span class="icon-dot"></span></a></li>
+                    </ul>
+                </li>
+                <li class="mobile-menu" id="submenu-down">
+                    <a href="#" class="line-down underline" id="submenu-down-two">Productos<ion-icon name="caret-down-outline"></ion-icon></a>
+                    <ul class="mobile-submenu">
+                        <li><a href="productos.html#2" class="underline">Equipos</a></li>
+                        <li><a href="productos.html#3" class="underline">Repuesto</a></li>
+                        <li><a href="productos.html#4" class="underline">Consumibles</a></li>
+                        <li><a href="productos.html#5" class="underline">Redes</a></li>
+                        <li><a href="productos.html#7" class="underline">Seguridad</a></li>
+                        <li><a href="productos.html#8" class="underline">Accesorios</a></li>
+                    </ul>
+                </li>
+                <li><a href="aboutme.html" class="underline">Nosotros</a></li>
+                <li><a href="contact.html" class="underline">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
