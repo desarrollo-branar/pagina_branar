423b8ab5fa16965b1f076431ef32bdaf83012f67
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-21 08:44:33 -0400

    uso de jquery

--- templates/header_blog.php
+++ templates/header_blog.php
@@ -13,7 +13,6 @@
 
     <!-- STYLES -->
 	<link rel="stylesheet" href="../assets/css/blog.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
     <link rel="stylesheet" href="../assets/css/navbar.css">
 
     <!-- FONT AWESONE -->
@@ -29,6 +28,8 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
     <!-- BOOTSTRAP CSS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 </head>
