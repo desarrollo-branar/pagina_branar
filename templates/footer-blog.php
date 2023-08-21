402199bd309a856b24758ea0adebc7a3f8b84d25
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-21 08:44:18 -0400

    footer blog

---
+++ templates/footer-blog.php
@@ -0,0 +1,18 @@
+    <!-- ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <!-- cdn Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 1500,
+            once: true
+        });
+    </script>
+    <!-- scripts -->
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>
