b00f71c5ed53717e5da6e86a31a1f8dd48db0367
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-21 11:13:48 -0400

    logica para editar un post

---
+++ src/controller/edit_post.php
@@ -0,0 +1,60 @@
+<?php
+
+use Branar\Blog\model\Navegation;
+use Branar\Blog\model\Author;
+use Branar\Blog\model\Post;
+use Branar\Blog\model\ImageUploader;
+
+$id = $params['id'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($id)) {
+    $title = Navegation::validateInput($_POST['title']);
+    $description = Navegation::validateInput($_POST['description']);
+    $category = Navegation::validateInput($_POST['category']);
+    $status = Navegation::validateInput($_POST['status']);
+    echo $status; echo '<br>';
+    date_default_timezone_set("America/Caracas");
+    $date = new DateTime();
+
+    $post = new Post($title, $description);
+    $author = new Author(
+        $_SESSION['user_data']['username'],
+        $_SESSION['user_data']['first_name'],
+        $_SESSION['user_data']['last_name'],
+        $_SESSION['user_data']['email']
+    );
+
+    $data_author = $author->getAuthorByUserId($_SESSION['user_data']['id']);
+    
+    echo 'fuera del if';
+    if (isset($_FILES["file"]) && $_FILES["file"]['size'] > 0) {
+        $image = new ImageUploader();
+        echo 'moviendo imagen';
+        $uploader = $image->uploadImage($_FILES["file"]);
+        
+        if ($uploader['response'] == true) {
+            $update_post = $post->updatePost($data_author['id'], $category, $_FILES["file"]['name'], $status, $date->format("Y-m-d H:i:s"), $id);
+
+            if ($update_post['response'] == true) {
+                echo 'actualizada';
+                dd($update_post);
+            }else{
+                echo 'algo salido mal al actualizar';
+                dd($update_post);
+            }
+        }else{
+            dd($uploader);
+        }
+
+    }else{
+        $update_post = $post->updatePost($data_author['id'], $category,'',$status, $date->format("Y-m-d H:i:s"), $id);
+
+        if ($update_post['response'] == true) {
+            echo 'actualizada';
+            dd($update_post);
+        }else{
+            echo 'algo salido mal al actualizar';
+            dd($update_post);
+        }
+    }
+}
