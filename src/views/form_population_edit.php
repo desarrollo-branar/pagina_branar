9f250ce2d68fd872ecf2cb46f6e22f04b270760b
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-21 08:41:49 -0400

    modal para editar post

---
+++ src/views/form_population_edit.php
@@ -0,0 +1,14 @@
+<!-- Modal -->
+<div class="modal fade" id="editPostModal-<?= $post['id'] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editPostModalLabel-<?= $post['id'] ?>" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editPostModalLabel">Edit Post</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Aquí deberías cargar los datos del post en el formulario de edición -->
+            </div>
+        </div>
+    </div>
+</div>
