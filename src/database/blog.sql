246e1bdf506e3cf6a888614b9a513e3b87d92df5
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-22 13:34:25 -0400

    nueva estructura de la base de datos

--- src/database/blog.sql
+++ src/database/blog.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
 --
 -- Host: localhost    Database: blog_db
 -- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.22.04.4
+-- Server version	8.0.34-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,13 +24,15 @@ DROP TABLE IF EXISTS `authors`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `authors` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` char(50) NOT NULL,
-  `email` char(100) NOT NULL,
   `user_id` int NOT NULL,
+  `specialization` varchar(100) DEFAULT NULL,
+  `picture` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT (now()),
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +41,7 @@ CREATE TABLE `authors` (
 
 LOCK TABLES `authors` WRITE;
 /*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1,1,'Redes, Progrmacion, Virtualizacion','','2023-08-17 17:59:19',NULL);
 /*!40000 ALTER TABLE `authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,8 +55,9 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` char(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_category` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +66,37 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (3,'Desarrollo Web'),(2,'Programación'),(5,'Redes Sociales'),(4,'Seguridad Informática'),(1,'Tecnología');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `content_posts`
+--
+
+DROP TABLE IF EXISTS `content_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `content_posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file` varchar(255) NOT NULL,
+  `post_id` int NOT NULL,
+  `status` char(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `content_posts_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `content_posts`
+--
+
+LOCK TABLES `content_posts` WRITE;
+/*!40000 ALTER TABLE `content_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `content_posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -75,8 +109,9 @@ DROP TABLE IF EXISTS `labels`;
 CREATE TABLE `labels` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` char(50) NOT NULL,
+  `color` char(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,6 +120,7 @@ CREATE TABLE `labels` (
 
 LOCK TABLES `labels` WRITE;
 /*!40000 ALTER TABLE `labels` DISABLE KEYS */;
+INSERT INTO `labels` VALUES (1,'Hacking Ético','red'),(2,'Redes Inalámbricas','blue'),(3,'Internet','yellow');
 /*!40000 ALTER TABLE `labels` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,6 +147,7 @@ CREATE TABLE `post_label` (
 
 LOCK TABLES `post_label` WRITE;
 /*!40000 ALTER TABLE `post_label` DISABLE KEYS */;
+INSERT INTO `post_label` VALUES (1,1),(1,2),(1,3),(2,3),(3,3),(4,3);
 /*!40000 ALTER TABLE `post_label` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,19 +160,23 @@ DROP TABLE IF EXISTS `posts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `posts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` char(70) NOT NULL,
-  `description` varchar(150) NOT NULL,
-  `release_date` date NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `author_id` int NOT NULL,
   `category_id` int NOT NULL,
-  `views` int NOT NULL,
+  `views` int DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT (false),
   `featured_image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT (now()),
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`),
   KEY `author_id` (`author_id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +185,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Seguridad digital','La seguridad digital se refiere a las medidas y prácticas diseñadas para proteger la información y los sistemas digitales de amenazas, ataques y accesos no autorizados.',1,4,NULL,'seguridad-digital.html',1,'','2023-08-17 18:11:02',NULL),(2,'Navegadores','Nos permiten acceder a todo tipo de información y realizar diversas tareas en línea, desde el computador o desde el telefono movil.',1,3,NULL,'navegadores.html',0,'navegadores.jpeg','2023-08-17 18:14:10','2023-08-21 14:25:46'),(3,'Digitalización de Empresas ','La digitalización de empresas es un proceso fundamental en la actualidad, ya que implica la transformación de los procesos y operaciones de una compañía hacia un entorno digital.',1,1,NULL,'digitalización-de-empresas-.html',1,'','2023-08-17 18:15:34',NULL),(4,'Intelegencia Artificial','La inteligencia artificial (IA) es un conjunto de tecnologías que permiten que las computadoras realicen una variedad de funciones avanzadas.',1,1,NULL,'intelegencia-artificial.html',1,'','2023-08-17 18:17:17',NULL),(7,'Recomendaciones para aplicar la IA en su empresa','Aplicar la Inteligencia Artificial en una empresa o negocio no se trata únicamente de escoger una herramienta de IA y pedirle al equipo que la utilice.',1,1,NULL,'recomendaciones-para-aplicar-la-ia-en-su-empresa.html',1,'Ia.jpeg','2023-08-18 15:43:00',NULL);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,8 +199,9 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` char(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_role` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,6 +210,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'interacting');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,13 +224,20 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` char(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
   `email` char(100) NOT NULL,
+  `password` char(16) DEFAULT NULL,
   `status` char(20) NOT NULL DEFAULT 'disable',
   `role_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT (now()),
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`),
+  UNIQUE KEY `unique_username` (`username`),
   KEY `role_id` (`role_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,6 +246,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'branar_admin','Branar','Creator','ander.dev@gmail.com','v-123456','enabled',1,'2023-08-17 17:52:22',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -207,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-11 15:30:52
+-- Dump completed on 2023-08-22 13:34:05
