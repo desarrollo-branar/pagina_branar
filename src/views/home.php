7fd046acd6ff1a58deedcf52c27d81f37f82bb28
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-23 07:40:20 -0400

    crear condicionales para modificar las cosas que puede ver y hacer el usuario admin y el interacting

--- src/views/home.php
+++ src/views/home.php
@@ -5,57 +5,143 @@ include_once './templates/navbar.php';
 use Branar\Blog\model\Navegation;
 use Branar\Blog\model\Post;
 use Branar\Blog\model\Label;
-
 ?>
 <main>
-    <h1>BLOG</h1>
     <div class="container">
         <header class="header_post_title">
-            <h2>Publicaciones</h2>
+            <h1>Blog</h1>
         </header>
+    <?php if( isset($_SESSION['user_data']) ): 
+        # Si existe una session ?>
+
+        <?php if($_SESSION['user_data']['role'] == 1  ): ?>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Create Post</button>
+        <?php endif; ?>
 
         <section class="container_post">
 
             <?php
 
-        $posts = Post::getAllThePublishingInformation();
-        foreach ($posts as $key => $post) {
-        if ($post['status'] == 0) { ?>
-            <article class="wrapper_post">
-                <header class="header_post" style="background-image: url(<?= $post['featured_image'] ? "../post_image/{$post['featured_image']}" : "../post_image/tecnology_image.jpeg" ?>);">
-                    <div>
-                        <h3><?= $post['title'] ?></h3>
+            $posts = Post::getAllThePublishingInformation();
+            foreach ($posts as $key => $post) { ?>
+                <article class="wrapper_post">
+                    <header class="header_post" style="background-image: url(<?= $post['featured_image'] ? "../post_image/{$post['featured_image']}" : "../post_image/tecnology_image.jpeg" ?>);">
+                        <div>
+                            <h3><?= $post['title'] ?></h3>
+                        </div>
+                    </header>
+                    <div class="content_post">
+                        <div class="labels">
+                            <?php
+                            $labels = Label::getPostLabelById($post['post_id']);
+                            foreach ($labels as $key => $label) { ?>
+                                <span class="<?= $label['color'] ?>"><?= $label['name'] ?></span>
+                            <?php } ?>
+                        </div>
+                        <p><?= $post['description'] ?></p>
                     </div>
-                </header>
-                <div class="content_post">
-                    <div class="labels">
-                        <?php
-                        $labels = Label::getPostLabelById($post['post_id']);
-                        foreach ($labels as $key => $label) { ?>
-                            <span class="<?= $label['color'] ?>"><?= $label['name'] ?></span>
-                        <?php   }
-                        ?>
+                    <div class="cont-buttons">
+                        <a href="../Blog/<?= $post['post_id'] ?>" class="btn btn-primary">Ver mas</a>
+
+                        <?php if ($_SESSION['user_data']['role'] == 1) { 
+                            # Mostrar boton para editar post si el usuario es admin
+                            ?>
+                            <a href="javascript:void(0);" class="edit-post-link btn btn-warning" data-post-id="<?= $post['post_id'] ?>" data-bs-target="#editPostModal-<?= $post['id'] ?>">Editar Post</a>
+                        <?php } ?>
+
                     </div>
-                    <p><?= $post['description'] ?></p>
-                </div>
-                <div class="cont-buttons">
-                    <a href="../Blog/<?= $post['post_id'] ?>" class="btn btn-primary">Ver mas</a>
-                </div>
-                <address class="info_author_post">
-                    <div>
-                        <p><span>Autor: </span><?= $post['first_name'] . ' ' . $post['last_name'] ?></p>
-                        <p><span>Fecha de publicacion: </span><?php $date = date_create($post['created_at']);
-                                                                echo date_format($date, "d-m-Y"); ?></p>
-                        <p><span><i class="fa-solid fa-eye"></i></span> <?= $post['views'] != null ? $post['views'] : '0' ?></p>
-                    </div>
-                </address>
-            </article>
-        <?php }
-        }
-        ?>
+                    <address class="info_author_post">
+                        <div>
+                            <p><span>Autor: </span><?= $post['first_name'] . ' ' . $post['last_name'] ?></p>
+                            <p><span>Fecha de publicacion: </span><?php $date = date_create($post['created_at']);
+                                                                    echo date_format($date, "d-m-Y"); ?></p>
+                            <p><span><i class="fa-solid fa-eye"></i></span> <?= $post['views'] != null ? $post['views'] : '0' ?></p>
+
+                            <?php if ($_SESSION['user_data']['role'] == 1) { 
+                            # Mostrar Status del Post si el usuario es admin
+                            ?>
+                            <p><span>Status: </span><?= $post['status'] == 0 ? 'Habilitado' : 'Deshabilitado' ?></p>
+                            <?php } ?>
+
+                        </div>
+                    </address>
+                </article>
+            <?php } ?>
         </section>
 
+        
+        <?php if($_SESSION['user_data']['role'] == 1){
+        # Incluir modales para crear y editar si el usuario es admin
+        require_once './src/views/form_population_create.php';
+        require_once './src/views/form_population_edit.php';
+        }
+        ?>
     </div>
+<?php else: ?>
+
+        <section class="container_post">
+
+            <?php
+
+            $posts = Post::getAllThePublishingInformation();
+            foreach ($posts as $key => $post) { ?>
+                <article class="wrapper_post">
+                    <header class="header_post" style="background-image: url(<?= $post['featured_image'] ? "../post_image/{$post['featured_image']}" : "../post_image/tecnology_image.jpeg" ?>);">
+                        <div>
+                            <h3><?= $post['title'] ?></h3>
+                        </div>
+                    </header>
+                    <div class="content_post">
+                        <div class="labels">
+                            <?php
+                            $labels = Label::getPostLabelById($post['post_id']);
+                            foreach ($labels as $key => $label) { ?>
+                                <span class="<?= $label['color'] ?>"><?= $label['name'] ?></span>
+                            <?php } ?>
+                        </div>
+                        <p><?= $post['description'] ?></p>
+                    </div>
+                    <div class="cont-buttons">
+                        <a href="../Blog/<?= $post['post_id'] ?>" class="btn btn-primary">Ver mas</a>
+                    </div>
+                    <address class="info_author_post">
+                        <div>
+                            <p><span>Autor: </span><?= $post['first_name'] . ' ' . $post['last_name'] ?></p>
+                            <p><span>Fecha de publicacion: </span><?php $date = date_create($post['created_at']);
+                                                                    echo date_format($date, "d-m-Y"); ?></p>
+                            <p><span><i class="fa-solid fa-eye"></i></span> <?= $post['views'] != null ? $post['views'] : '0' ?></p>
+                        </div>
+                    </address>
+                </article>
+            <?php } ?>
+        </section>
+    </div>
+<?php endif; ?>
 </main>
 
-<?php require_once './templates/footer.php'; ?>
+<script>
+    $(document).ready(function() {
+        // Manejar el clic en el enlace "Editar Post"
+        $('.edit-post-link').click(function() {
+            var postId = $(this).data('post-id');
+            var modalId = $(this).data('bs-target'); // Obtener el ID del modal
+            var url = './get_data_post/' + postId; // Construir la URL
+
+            $.ajax({
+                url: url,
+                method: 'GET',
+                success: function(response) {
+                    console.log(response)
+                    $(modalId + ' .modal-content').html(response); // Usar el ID del modal para abrir el modal correcto
+                    $(modalId).modal('show');
+                },
+                error: function() {
+                    console.log(response);
+                    alert('Error al cargar los datos del post.');
+                }
+            });
+        });
+    });
+</script>
+
+<?php require_once './templates/footer-blog.php'; ?>
