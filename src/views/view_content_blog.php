46327c604b72bd53082afad71e0576c4c627975e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-29 13:41:09 -0400

    agregar seccion de comentarios

--- src/views/view_content_blog.php
+++ src/views/view_content_blog.php
@@ -7,7 +7,7 @@ use Branar\Blog\model\Post;
 use Branar\Blog\model\Category;
 use Branar\Blog\model\Label;
 use Branar\Blog\functions\UtilFunctions;
-use JetBrains\PhpStorm\Internal\PhpStormStubsElementAvailable;
+use Branar\Blog\model\Comment;
 
 $id = Navegation::validateInput($params['id']);
 $post = Post::getPostById($id);
@@ -85,16 +85,49 @@ $post = Post::getPostById($id);
             </form>
 
         </aside>
+
+        <section class="blog_comment container">
+            <header class="blog_comment_header">
+                <h2 style="color: black;">Comentarios</h2>
+            </header>
+            <div class="comments">
+                <!-- Mostrar comentarios existentes -->
+                <?php 
+                    $comments = Comment::get_all_comment_data_by_post_id($params['id']);
+                            
+                    if(count($comments) > 0): foreach ($comments as $comment): ?>
+                    <div class="comment mt-4">
+                        <header class="comment_header">
+                            <p><strong><?= "{$comment['first_name']} {$comment['last_name']}" ?></strong></p>
+                            <p><?= $comment['created_at'] ?></p>
+                        </header>
+                        <div class="content_comment">
+                            <p class="comment_text"><?= $comment['content'] ?></p>
+                        </div>
+                    </div>
+                <?php  endforeach; else: ?>
+                    <div class="comment">
+                        <p class="comment_author">Se el primero en comentar</p>
+                    </div>
+                <?php endif; ?>
+
+                <?php if(isset($_SESSION['user_data'])):?>
+                <!-- Formulario para escribir un nuevo comentario -->
+                <div class="new_comment mt-4">
+                    <h3>Deja un comentario</h3>
+                    <form action="../blog/submit_comment" method="POST">
+                        <div class="form-group">
+                            <label for="comment_content">Comentario:</label>
+                            <textarea name="comment_content" class="form-control" rows="3" required></textarea>
+                        </div>
+                        <input type="hidden" name="post_id" value="<?= $params['id'] ?>">
+                        <button type="submit" class="btn btn-primary">Enviar comentario</button>
+                    </form>
+                </div>
+                <?php endif; ?>
+            </div>
+        </section>
     </div>
-
-    <section class="blog_comment">
-        <header class="blog_comment_header">
-            <h2 style="color: black;">Comentarios</h2>
-        </header>
-        <div>
-
-        </div>
-    </section>
 
 </main>
 
