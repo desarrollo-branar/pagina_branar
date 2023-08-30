32171c249d1df575157998619d094130e0e197bb
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-08-30 13:40:59 -0400

    cambios en el footer

--- templates/footer-blog.php
+++ templates/footer-blog.php
@@ -1,4 +1,68 @@
-    <!-- ionicons -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <ul>
+                        <li>
+                            <a href="../">
+                                <img src="../assets/images/logo-para-fondo-oscuro.png" class="image_footer" alt="Branar - Servicios en Tecnología">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer_container">
+            <div class="footer_column">
+                <header>
+                    <h3>Unete a nosotros</h3>
+                </header>
+                <ul>
+                    <li><a href="../service/csa" class="underline-footer">Csa Epson<span class="icon-dot"></span></a></li>
+                    <li><a href="../service/infraestructure" class="underline-footer">Infraestructura<span class="icon-dot"></span></a></li>
+                    <li><a href="../service/socialmedia" class="underline-footer">SocialMedia<span class="icon-dot"></span></a></li>
+                    <li><a href="../service/profitplus" class="underline-footer">Profit Plus<span class="icon-dot"></span></a></li>
+                    <li><a href="../service/servipack" class="underline-footer">ServiPack<span class="icon-dot"></span></a></li>
+                    <li><a href="../service/web-development" class="underline-footer">Desarrollo Web<span class="icon-dot"></span></a></li>
+                </ul>
+            </div>
+            <div class="footer_column">
+                <header>
+                    <h3>Nosotros</h3>
+                </header>
+                <ul>
+                    <li><a href="../about/" class="underline-footer">Quienes somos?<span class="icon-dot"></span></a></li>
+                </ul>
+            </div>
+            <div class="footer_column">
+                <header>
+                    <h3>Soporte</h3>
+                </header>
+                <ul>
+                    <li><a href="../contact/" class="underline-footer">Contactanos<span class="icon-dot"></span></a></li>
+                </ul>
+            </div>
+            <div class="footer_column">
+                <header>
+                    <h3>Soporte</h3>
+                </header>
+                <div>
+                    <p>Teléfono: (0251) – 4453083 / 0412 - 3290719</p>
+                    <p>Dirección: Av. Libertador, Cruce Calle 41, Local 41-12. Barquisimeto-Lara.</p>
+                    <p>Correo: contacto@branar.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-sm-12 d-flex justify-content-center">
+                    <p style="color: #6c6c6c;">Branar, C.A. J-310451397 © Copyright 2023 Todos los derechos reservados.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+   <!-- ionicons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
