3058f71e005db7c405abf2efb4bc8240e4109520
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-06-26 16:11:40 -0400

    configurar packages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import copy from 'rollup-plugin-copy';
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+
+        copy({
+            targets: [
+                // Copia los archivos necesarios de Glide.js desde node_modules a tu carpeta pública
+                { src: 'node_modules/@glidejs/glide/dist/css/glide.core.min.css', dest: 'public/css' },
+                { src: 'node_modules/@glidejs/glide/dist/css/glide.theme.min.css', dest: 'public/css' },
+                { src: 'node_modules/@glidejs/glide/dist/glide.min.js', dest: 'public/js' },
+            ],
         }),
     ],
 });
