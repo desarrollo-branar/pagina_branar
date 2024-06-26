07ebac2abc5b439ed9f2a1e56d37bc77e4fcf2dd
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-06-26 16:11:26 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('index');
+Route::get('about-us/', [PageController::class, 'about'])->name('about');
+Route::get('contact/', [PageController::class, 'contact'])->name('contact');
