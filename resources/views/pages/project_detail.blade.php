e0dc1c76b23734a6d580b27c34bc5f2b0ba266c4
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-06-29 19:23:22 -0400

    project detail

--- resources/views/pages/project_detail.blade.php
+++ resources/views/pages/project_detail.blade.php
@@ -5,9 +5,32 @@
 @endsection
 
 @section('content')
-  <h1>{{ $project->name }}</h1>
+  <section class="project_bg_top" style="background-image: url({{ asset('images/banfondo.jpg') }});">
+    <header>
+      <h1>{{ $project->name }}</h1>
+      <p>{{ $project->description }}</p>
+    </header>
+  </section>
+
+  <section class="project_details">
+    <article class="detail">
+      <div class="detail_image">
+        <img src="{{ asset($project->image) }}" alt="{{ $project->name }}">
+      </div>
+      <div class="detail_content">
+        <header>
+          <h2>Descripción del proyecto</h2>
+        </header>
+        <div>
+          <p>{{ $project->description }}</p>
+        </div>
+      </div>
+    </article>
+  </section>
+
+  {{-- <h1>{{ $project->name }}</h1>
   <img src="{{ asset($project->image) }}" alt="{{ $project->name }}">
   <p>{{ $project->description }}</p>
   <h3>Features:</h3>
-  <p>{{ $project->features }}</p>
+  <p>{{ $project->features }}</p> --}}
 @endsection
