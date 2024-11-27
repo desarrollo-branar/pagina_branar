b7d877aa02c9466c4fbcc1f0658fce6dcc28cd14
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-11-27 13:31:59 -0400

    robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  modules: [
+    ['@nuxtjs/robots', {
+      rules: [
+        {
+          userAgent: '*', // Permite acceso a todos los bots
+          allow: '/', // Permitir el acceso a todo el sitio
+          disallow: '/admin', // Bloquear la ruta `/admin`
+        }
+      ],
+      sitemap: 'https://tusitio.com/sitemap.xml', // URL del sitemap
+    }],
+  ],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
